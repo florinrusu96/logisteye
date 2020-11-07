1c53a35b36a93b49518812489d43b09c4fdaed65
florinrusu96 <rusuflorinandrei96@gmail.com>
2020-11-07 05:41:43 +0200

    Master instance

--- src/communication/models.py
+++ src/communication/models.py
@@ -5,3 +5,4 @@ class Instance(models.Model):
     company = models.ForeignKey("rest_api.Company", on_delete=models.CASCADE)
     instance_url = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
+    is_master_instance = models.BooleanField(default=False, editable=False)
