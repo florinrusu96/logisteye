ac7069e8047e877ea09f15761900682b7917c175
florinrusu96 <rusuflorinandrei96@gmail.com>
2020-11-07 11:55:54 +0200

    User cica login

--- src/rest_api/models/user.py
+++ src/rest_api/models/user.py
@@ -7,17 +7,20 @@ from rest_api.managers.user_manager import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ("ADMIN", 'admin'),
+        ("DELIVERY", 'delivery')
+    )
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    free_user = models.BooleanField(default=False)
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
         help_text=_('Designates whether the user can log into this admin site.'),
     )
     assigned_area = models.OneToOneField("Area", on_delete=models.DO_NOTHING, null=True)
-
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="DELIVERY")
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
